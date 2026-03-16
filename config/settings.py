1cd68002883298af04565cda732be7b0cb3bb7aa
fabricio00101 <jfmilanesio@gmail.com>
2026-03-15 21:33:15 -0300

    Fix DEFAULT_AUTO_FIELD warnings

--- config/settings.py
+++ config/settings.py
@@ -118,3 +118,8 @@ STATIC_URL = 'static/'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
